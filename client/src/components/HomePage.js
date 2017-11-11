9a4d048d9727ab45f947ebae2800a62b4eb46597
jamespeterson <jamespeterson0918@yahoo.com>
2017-11-11 14:40:08 -0500

    added background for home page

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -1,19 +1,27 @@
 import React, { Component } from 'react';
 import styled from 'styled-components'
 
-
+const FlexContainer = styled.div`
+display: flex;
+justify-content: space-around;
+padding: 150px;
+float: center;
+`
 
 
 class HomePage extends Component {
     render() {
         return (
 
-             
+            <FlexContainer> 
             <div>
-                <img src="https://i.imgur.com/zkSEnuE.jpg"/> 
+                <iframe width="420" height="315"
+            src="https://www.youtube.com/embed/Tm8LGxTLtQk">
+                </iframe>
             </div>
-            
-        );
+        </FlexContainer>
+
+        )
     }
 }
 
